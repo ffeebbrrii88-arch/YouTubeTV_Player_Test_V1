2eab7e458c0a76709f4bd012319c4f23ef818881
ffeebbrrii88-arch <ffeebbrrii88@gmail.com>
2026-09-08 18:52:34 +0700

    Add NewPipe Extractor dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,15 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+    // ExoPlayer
     implementation("com.google.android.exoplayer:exoplayer:2.19.1")
+
+    // NewPipe Extractor
+    implementation("com.github.TeamNewPipe:NewPipeExtractor:v0.24.0")
+
+    // Downloader
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
